aea8fc16c5e8adbf08e9ff09aabfa6dcdfef28a7
Karsten Kraus <Karsten.Kraus@web.de>
2018-08-02 23:50:45 +0200

    Cleanup

--- src/test/groovy/argelbargel/gradle/plugins/TestUtility.groovy
+++ src/test/groovy/argelbargel/gradle/plugins/TestUtility.groovy
@@ -1,62 +1,15 @@
 package argelbargel.gradle.plugins
 
-import org.gradle.api.Project
-import org.gradle.api.internal.project.ProjectInternal
 import org.gradle.internal.classpath.ClassPath
 import org.gradle.internal.classpath.DefaultClassPath
 import org.gradle.testkit.runner.BuildResult
 import org.gradle.testkit.runner.GradleRunner
 import org.gradle.testkit.runner.internal.PluginUnderTestMetadataReading
 
-import java.util.concurrent.atomic.AtomicBoolean
-
 import static java.util.Arrays.asList
-import static org.apache.commons.lang3.StringUtils.isNotBlank
 
 final class TestUtility {
 
-    static void evaluate(Project project) {
-        AtomicBoolean done = new AtomicBoolean(false)
-        project.afterEvaluate {
-            done.set(true)
-            synchronized (done) {
-                done.notify()
-            }
-        }
-
-        (project as ProjectInternal).evaluate()
-
-        synchronized (done) {
-            while (!done.get()) {
-                wait(10)
-            }
-        }
-    }
-
-    static void resolve(Project project) {
-        evaluate(project)
-        (project as ProjectInternal).configurations.findAll { it.canBeResolved }.each { it.resolve() }
-    }
-
-    static void prepareProject(File projectDir, String buildScript, String settings = '') {
-        projectDir.mkdirs()
-        new File(projectDir, "build.gradle").text = """
-            buildscript {
-                ${pluginUnderTestDependencies()}
-            }
-
-            ${buildScript}
-        """
-        
-        if (isNotBlank(settings)) {
-            new File(projectDir, "settings.gradle").text = createSettings(settings)
-        }
-    }
-
-    static BuildResult buildScript(File projectDir, String buildScript, String... arguments) {
-        prepareProject(projectDir, buildScript)
-        buildProject(projectDir, arguments)
-    }
 
     static BuildResult buildProject(File projectDir, String... arguments) {
         GradleRunner.create()
