115a1dfb6f42875e2cc7dfeb164d51898f3847f6
Karsten Kraus <Karsten.Kraus@web.de>
2018-08-03 00:02:25 +0200

    gradlew must be executable

--- travis/install.sh
+++ travis/install.sh
@@ -2,11 +2,13 @@
 
 echo "Building branch $TRAVIS_BRANCH (pull-request: $TRAVIS_PULL_REQUEST)..."
 
+
 GRADLE_TASKS="test"
 if [ "$TRAVIS_TAG" != "" ]; then
     GRADLE_TASKS="$GRADLE_TASKS publish"
 fi
 
+chmod +x ./gradlew
 ./gradlew ${GRADLE_TASKS} -Prelease=${TRAVIS_TAG} --no-daemon
 
 if [ "$TRAVIS_TAG" != "" ]; then
